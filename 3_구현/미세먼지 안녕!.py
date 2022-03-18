fe60e2fd65ac0d4e90169de48be118d417522ce6
joyful0920 <geunseong0920@gmail.com>
2022-03-19 06:20:55 +0900

    Upload source codes

--- 3_구현/미세먼지 안녕!.py
+++ 3_구현/미세먼지 안녕!.py
@@ -2,54 +2,87 @@ import sys
 si = sys.stdin.readline
 
 r, c, t = map(int, si().split())
-array = [list(map(int, si().split())) for _ in range(r)]
-copy = [[0] * c for _ in range(r)]
+graph = [list(map(int, si().split())) for _ in range(r)]
 
+# 공기청정기 위치 구하기
 for i in range(r):
-    if array[i][0] == -1:
+    if graph[i][0] == -1:
         up = i
         down = i + 1
-        copy[i][0] = -1
+        break
 
-def pm(i, j):
-    copy = [[0] * c for _ in range(r)]
-    m = array[i][j] // 5
-    rest = array[i][j]
-    if 0 < i - 1:
-        if copy[i - 1][j] != -1:
-            copy[i - 1][j] += m
-            rest -= m
-    if i + 1 < r:
-        if copy[i + 1][j] != -1:
-            copy[i + 1][j] += m
-            rest -= m
-    if 0 < j - 1:
-        if copy[i][j - 1] != -1:
-            copy[i][j - 1] += m
-            rest -= m
-    if j + 1 < c:
-        if copy[i][j + 1] != -1:
-            copy[i][j + 1] += m
-            rest -= m
-    copy[i][j] += rest
-    array = copy
+# 미세먼지 확산 함수
+def pm():
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+    temp = [[0] * c for _ in range(r)]
 
-def cleaner():
-    copy = [[0] * c for _ in range(r)]
-    for i in range(1, c):
-        copy[0][i - 1] = array[0][i]
-        copy[up][i + 1] = array[up][i]
-    for i in range(1, c):
-        copy[down][i + 1] = array[down][i]
-        copy[r - 1][i - 1] = array[r - 1][i]
-    for i in range(0, up - 1):
-        copy[i + 1][0] = array[i][0]
-    for i in range(0, up):
-        copy[i + 1][0] = array[i][0]
-
-    
-for _ in range(t):
     for i in range(r):
         for j in range(c):
-            pm(i, j)
+            if graph[i][j] != 0 and graph[i][j] != -1:
+                cnt = 0
+                for k in range(4):
+                    nx = i + dx[k]
+                    ny = j + dy[k]
+                    if 0 <= nx < r and 0 <= ny < c and graph[nx][ny] != -1:
+                        temp[nx][ny] += graph[i][j] // 5
+                        cnt += 1
+                graph[i][j] -= (graph[i][j] // 5 * cnt)
     
+    for i in range(r):
+        for j in range(c):
+            graph[i][j] += temp[i][j]
+
+# 공기청정기 위쪽 방향의 흐름
+def cleaner_up():
+    # 반시계 방향
+    dx = [0, -1, 0, 1]
+    dy = [1, 0, -1, 0]
+    direct = 0
+    before = 0
+    x, y = up, 1
+    while True:
+        nx = x + dx[direct]
+        ny = y + dy[direct]
+        if x == up and y == 0:
+            break
+        if nx < 0 or nx >= r or ny < 0 or ny >= c:
+            direct += 1
+            continue
+        # 이렇게 한줄로 표현하면 temp 없이 두값 교환 가능
+        graph[x][y], before = before, graph[x][y]
+        x = nx
+        y = ny
+
+# 공기청정기 아래쪽 방향의 흐름
+def cleaner_down():
+    # 시계 방향
+    dx = [0, 1, 0, -1]
+    dy = [1, 0, -1, 0]
+    direct = 0
+    before = 0
+    x, y = down, 1
+    while True:
+        nx = x + dx[direct]
+        ny = y + dy[direct]
+        if x == down and y == 0:
+            break
+        if nx < 0 or nx >= r or ny < 0 or ny >= c:
+            direct += 1
+            continue
+        graph[x][y], before = before, graph[x][y]
+        x = nx
+        y = ny
+
+for _ in range(t):
+    pm()
+    cleaner_up()
+    cleaner_down()
+
+answer = 0
+for i in range(r):
+    for j in range(c):
+        if graph[i][j] > 0:
+            answer += graph[i][j]
+
+print(answer)
